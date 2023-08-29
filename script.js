2a4a9fd7a0ef7618bcad861f0d25e1f6f039657e
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-08-29 08:39:14 -0400

    Create skeleton code with step-by-step solution in comments'

---
+++ script.js
@@ -0,0 +1,45 @@
+function getComputerChoice() {
+    // Returns a (random) move for the computer
+
+    // Create array of possible choices
+    // Generate random number for array index
+    // Extract choice from array using the random index
+    // Return choice
+}
+
+function getUserChoice() {
+    // Prompts the user to make their choice
+
+    // Prompt user for choice
+    // Validate user choice
+    // Return user choice
+}
+
+function playRound(userChoice, computerChoice) {
+    // Returns a string with the round results
+
+    // Compare choices to determine winner
+        // If tie, do not increment scores
+        // Else add 1 to score of winner
+    // Return round results
+}
+
+function endGame() {
+    // Ends the current game and asks user if they'd like to play again
+
+    // Report results of game
+    // Ask if user would like to play again
+    // If user wants to play again. Start new game
+    // Else exit program
+}
+
+function game() {
+    // Runs a best-of-5 game and returns the game results
+
+    // Reset scores to 0
+    // While both scores are less than 3
+        // Get a user choice and computer choice
+        // Play a round
+        // Display round results
+    // End the current game
+}
